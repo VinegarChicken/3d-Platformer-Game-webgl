af1f1beba1c3bf1ea70c8570482f88c2ffa15f06
Texpert-GD <65292910+Texpert-GD@users.noreply.github.com>
2020-09-15 15:35:48 -0400

    Added explosion effect 👏

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
